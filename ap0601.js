11355e1f637e39d61161120b0664933ede9940dd
Takashi MINOHARA <minohara@cs.takushoku-u.ac.jp>
2022-11-01 13:26:23 +0900

    初期コード

--- ap0601.js
+++ ap0601.js
@@ -5,15 +5,17 @@
 "use strict"; // 厳格モード
 
 import * as THREE from 'three';
+import { TrackballControls } from 'control';
 import GUI from 'gui';
 
 // ３Ｄページ作成関数の定義
 function init() {
   const controls = {
-    fov: 50, // 視野角
-    hight: 3,
     rotation: 2,
-    axes: true
+    axes: true,
+    reset: function() {
+      // カメラの制御をリセットする
+    }
   };
 
   // シーン作成
@@ -21,92 +23,62 @@ function init() {
 
   // カメラの設定
   const camera = new THREE.PerspectiveCamera(
-    controls.fov, window.innerWidth/window.innerHeight, 0.1, 1000);
+    80, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.set(3,2,4);
+  camera.lookAt(0,1,0);
 
   // レンダラの設定
   const renderer = new THREE.WebGLRenderer();
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  renderer.setClearColor( 0x406080 );
-  renderer.shadowMap.enabled = true;
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setClearColor(0x406080);
+  // レンダラに影の処理をさせる
+  
+  // Webページに描画領域を対応させる
   document.getElementById("WebGL-output")
     .appendChild(renderer.domElement);
+
+  // カメラコントロール
 
   // 座標軸の設定
   const axes = new THREE.AxesHelper(18);
   scene.add(axes);
-  
-   // テクスチャの読み込み
-   const textureLoader = new THREE.TextureLoader();
-   const texture = textureLoader.load("logo.png");
-   const venderTexture01 = textureLoader.load("vender01.jpg");
-   const venderTexture02 = textureLoader.load("vender02.jpg");
-  
-   // 立方体の作成
-   const cubeGeometry = new THREE.BoxGeometry(2, 2, 2);
-   const cubeMaterial = new THREE.MeshLambertMaterial();
-   // マテリアルにテクスチャを登録
-   cubeMaterial.map = texture;
-   const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-   cube.position.y = 2;
-   cube.position.x = -3;
-   cube.castShadow = true;
-   scene.add(cube);
 
-   const venderMaterial = new THREE.MeshPhongMaterial({color: 0xE8E7E5 });
-   const vender01Material = new THREE.MeshPhongMaterial({map: venderTexture01});
-   const vender02Material = new THREE.MeshPhongMaterial({map: venderTexture02});
-   
-   const vender01 = new THREE.Mesh(
-    new THREE.BoxGeometry(1.2, 1.8, 0.9),
-    [
-      venderMaterial, // 左
-      venderMaterial, // 右
-      venderMaterial, // 上
-      venderMaterial, // 下
-      vender01Material, // 前
-      venderMaterial, // 後
-    ]
-   )
-   vender01.castShadow = true;
-   vender01.receiveShadow = true;
-   vender01.position.y = vender01.geometry.parameters.height/2;
-   vender01.position.x = -vender01.geometry.parameters.width/2;
-   scene.add(vender01);
- 
-   const vender02 = new THREE.Mesh(
-    new THREE.BoxGeometry(1.0, 1.8, 0.9),
-    [
-      venderMaterial, // 左
-      venderMaterial, // 右
-      venderMaterial, // 上
-      venderMaterial, // 下
-      vender02Material, // 前
-      venderMaterial, // 後
-    ]
-   )
-   vender02.castShadow = true;
-   vender02.receiveShadow = true;
-   vender02.position.y = vender02.geometry.parameters.height/2;
-   vender02.position.x = vender02.geometry.parameters.width/2;
-   scene.add(vender02);
+  // テクスチャの読み込み
 
-   // 球の作成
-   const sphereGeometry = new THREE.SphereGeometry(1, 24, 24);
-   const sphereMaterial = new THREE.MeshPhongMaterial();
-   //sphereMaterial.map = texture2;
-   const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-   sphere.position.y = 2;
-   sphere.position.x = 3;
-   sphere.castShadow = true;
-   scene.add(sphere);
- 
-   // 平面の作成
-   const circle = new THREE.Mesh(
-     new THREE.CircleGeometry(20,24),
-     new THREE.MeshLambertMaterial({color:0x008010}));
-   circle.rotation.x = -Math.PI/2;
-   circle.receiveShadow = true;
-   scene.add(circle);
+  // 立方体の作成
+  const cubeGeometry = new THREE.BoxGeometry(2, 2, 2);
+  const cubeMaterial = new THREE.MeshNormalMaterial();
+  const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+  // 立方体にテクスチャを登録
+
+  // 立方体の位置
+  cube.position.y = 2;
+  cube.position.x = -3;
+  // 立方体は影を作る
+
+  // シーンに立方体を加える
+  scene.add(cube);
+
+  // 球の作成
+
+  // 球にテクスチャを登録
+
+  // 球の位置
+  
+  // 球は影を作る
+  
+  // シーンに球を加える
+  
+
+  // 平面の作成
+  const circle = new THREE.Mesh(
+    new THREE.CircleGeometry(20, 24),
+    new THREE.MeshLambertMaterial({ color: 0x008010 }));
+  circle.rotation.x = -Math.PI / 2;
+  // 平面は影を受け止める
+
+  // シーンに平面を加える
+  scene.add(circle);
 
   // 光源の作成
   const light = new THREE.DirectionalLight();
@@ -114,29 +86,64 @@ function init() {
   light.castShadow = true;
   scene.add(light);
 
+  // 自動販売機の作成
+  // 素材に関する処理
+  const venderMaterial = new THREE.MeshPhongMaterial({ color: 0xE8E7E5 });
+  
+  // 自動販売機1号
+  const vender01 = new THREE.Mesh(
+    new THREE.BoxGeometry(1.2, 1.8, 0.9),
+    [
+      // 面ごとの素材の設定
+      venderMaterial, // 左
+      venderMaterial, // 右
+      venderMaterial, // 上
+      venderMaterial, // 下
+      venderMaterial, // 前
+      venderMaterial, // 後
+    ]
+  )
+  // 自販機の影の設定
+  
+  // 自販機の位置の設定
+  
+  // 自販機をシーンに追加する
+  
+  // 自動販売機2号
+  const vender02 = new THREE.Mesh(
+    new THREE.BoxGeometry(1.0, 1.8, 0.9),
+    [
+      // 面ごとの素材の設定
+    ]
+  )
+  // 自販機の影の設定
+  
+  // 自販機の位置の設定
+  
+  // 自販機をシーンに追加する
+  
   // 描画関数の定義
+
   function render() {
+    // カメラ制御の更新
+    
+    // 座標軸のON/OFF
     axes.visible = controls.axes;
-    camera.fov = controls.fov;
-    camera.position.set(7, controls.hight, 5);
-    camera.lookAt(0, 1, 0);
-    camera.updateProjectionMatrix();
     // 物体の回転
-    cube.rotation.y += 0.01*controls.rotation;
-    sphere.rotation.y += 0.01*controls.rotation;
-    renderer.shadowMap.enabled = true;
+    cube.rotation.y += 0.01 * controls.rotation;
+    // 描画
     renderer.render(scene, camera);
+    // アニメーション
     requestAnimationFrame(render);
   }
 
   // GUIコントローラ
   const gui = new GUI();
-  gui.add(controls, "fov", 10, 100);
-  gui.add(controls, "hight", -10, 10);
   gui.add(controls, "rotation", -10, 10);
   gui.add(controls, "axes");
+  gui.add(controls, "reset");
 
-  // 描画
+  // 最初の描画
   render();
 }
 
